3bca81300a2762adbba3e0bf6063e245b74a353a
Vu Duc Ly <vuducly151092@gmail.com>
2017-09-19 04:23:17 -0400

    Create cautrucduclieu.py

---
+++ Code/cautrucduclieu.py
@@ -0,0 +1,27 @@
+
+from collections import Counter
+danhsachso = [20, 30 , 40]
+danhsachten = ["Tat", "AThai", "Thi"]
+#print("Danh sach: {}".format(danhsachso))
+
+# for i in danhsachso:
+# 	i = i + 10
+# 	print(i)
+
+# for ten in danhsachten:
+# 	if ten == "Tat":
+# 		print("Mr." + ten)
+# 	print(ten)
+
+# "Tat"  + str(22)
+
+# bhk = {"Ly": 25, "Tat":22}
+# print(bhk.keys())
+
+# print("Tuoi cua Ly: {}".format(bhk["Ly"]))
+
+mang_so = [1, 1,2,3,4, 1,4,5,1,7,8,9]
+mang_so_2 = [10, 11]
+print(Counter(mang_so))
+print(type(mang_so))
+print(mang_so + mang_so_2)[0]
